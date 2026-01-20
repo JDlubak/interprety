0cb9bc2480625e95b8c22c4c28e9ef214255794b
JDlubak <kuba.dl@wp.pl>
2026-01-20 17:41:28 +0100

    Login logout

---
+++ 4/order_app/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleLogout = () => {
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+  router.push('/login');
+};
+</script>
+
+<template>
+  <div class="container mt-5" style="max-width: 600px;">
+    <div class="card text-center">
+      <div class="card-body">
+        <h1 class="card-title">Hello, World!</h1>
+        <p class="card-text">Witaj po zalogowaniu!</p>
+        <button class="btn btn-danger mt-3" @click="handleLogout">
+          Logout
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ 4/order_app/src/views/LoginView.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { login } from '../services/authService';
+
+const loginField = ref('');
+const password = ref('');
+const error = ref(null);
+const loading = ref(false);
+
+const router = useRouter()
+
+const handleLogin = async () => {
+  error.value = null;
+  loading.value = true;
+  try {
+    const data = await login({login: loginField.value, password: password.value});
+    localStorage.setItem('accessToken', data.accessToken);
+    localStorage.setItem('refreshToken', data.refreshToken);
+    router.push('/');
+  } catch(err) {
+    error.value = err;
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<template>
+  <div class="container mt-5" style="max-width: 400px;">
+    <h1 class="text-center mb-4">Login</h1>
+    <form @submit.prevent="handleLogin">
+      <div class="mb-3">
+        <label for="login" class="form-label">Email or Username</label>
+        <input
+            type="text"
+            id="login"
+            class="form-control"
+            v-model="loginField"
+            placeholder="Enter your email or username"
+            required
+        />
+      </div>
+
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input
+            type="password"
+            id="password"
+            class="form-control"
+            v-model="password"
+            placeholder="Enter your password"
+            required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-success w-100" :disabled="loading">
+        {{ loading ? "Logging in..." : "Login" }}
+      </button>
+
+      <div v-if="error" class="alert alert-danger mt-3 text-center">
+        {{ error }}
+      </div>
+    </form>
+  </div>
+</template>
